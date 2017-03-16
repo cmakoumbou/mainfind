fe72fadc4479a15946e94216f8951116f556d282
Chris Makoumbou <cmakoumbou@gmail.com>
2017-03-16 17:04:10 +0000

    Code refactor and test refactor for the Riot Api

--- lib/riotapi.rb
+++ lib/riotapi.rb
@@ -1,14 +1,14 @@
 class RiotApi
   BASE_URL = "https://euw.api.pvp.net"
   RIOT_API_KEY = ENV['RIOT_API_KEY']
-  API_KEY = "&api_key=#{RIOT_API_KEY}"
 
   def get_challenger_league
-    challenger_league_url = "#{BASE_URL}/api/lol/EUW/v2.5/league/challenger?type=RANKED_SOLO_5x5#{API_KEY}"
+    challenger_league_url = "#{BASE_URL}/api/lol/EUW/v2.5/league/challenger?type=RANKED_SOLO_5x5&api_key=#{RIOT_API_KEY}"
     get_response(challenger_league_url)
   end
 
   def get_response(url)
     response = RestClient.get(url)
+    JSON.parse(response)
   end
 end
--- spec/lib/riotapi_spec.rb
+++ spec/lib/riotapi_spec.rb
@@ -9,7 +9,8 @@ describe 'Riot Api' do
     VCR.use_cassette('riotapi/get_challenger_league') do
       client = RiotApi.new
       response = client.get_challenger_league
-      expect(response.code).to eq(200)
+      expect(response['tier']).to eq('CHALLENGER')
+      expect(response['entries'].size).to eq(200)
     end
   end
 end
